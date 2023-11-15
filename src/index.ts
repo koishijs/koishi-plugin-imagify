d931d6f9d9f1db668152b9aad06b7d5e80852a90
Lipraty <sis1943@outlook.com>
2023-11-15 09:13:37 +0800

    update configs

--- src/index.ts
+++ src/index.ts
@@ -19,14 +19,14 @@ export interface Config {
 }
 
 export const Config: Schema<Config> = Schema.object({
-  maxLineCount: Schema.number().min(1).default(20).description('当文本行数超过该值时转为图片'),
-  maxLength: Schema.number().min(1).default(600).description('当返回的文本字数超过该值时转为图片'),
+  maxLineCount: Schema.number().min(1).default(20).description('当文本行数超过该值时转为图片').disabled(),
+  maxLength: Schema.number().min(1).default(648).description('当返回的文本字数超过该值时转为图片'),
   background: Schema.string().role('link').description('背景图片地址，以 http(s):// 开头'),
   blur: Schema.number().min(1).max(50).default(10).description('文本卡片模糊程度'),
-  style: Schema.string().role('textarea').default(css).description('文本卡片样式'),
+  style: Schema.string().role('textarea').default(css).description('文本卡片样式').experimental(),
 })
 
-export const using = ['puppeteer']
+export const inject = ['puppeteer']
 
 export function apply(ctx: Context, config: Config) {
   ctx.before('send', async (session) => {
