2863f79b161690f773f054e8adaded11a6fd0cb9
Lipraty <sis1943@outlook.com>
2023-11-25 00:02:03 +0800

    feat: (TODO) Support fastify model

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Context, Schema, h, version as kVersion } from 'koishi'
 import { } from 'koishi-plugin-puppeteer'
+import type { Page } from 'puppeteer-core'
 import { readFileSync } from 'fs'
 import { readFile } from 'fs/promises'
 import { appendElements, parser } from './parse'
@@ -12,25 +13,28 @@ const css = readFileSync(require.resolve('./default.css'), 'utf8')
 export const name = 'imagify'
 
 export interface Config {
+  fastify: boolean
+  advanced: boolean
+  rules: ImageRule[]
   maxLineCount: number
   maxLength: number
   background: string
   blur: number
   style: string
-  advanced: boolean
-  rules: ImageRule[]
 }
 
 export const Config: Schema<Config> = Schema.intersect([
   Schema.object({
-    maxLineCount: Schema.number().min(1).default(20).description('当文本行数超过该值时转为图片').disabled(),
-    maxLength: Schema.number().min(1).default(648).description('当返回的文本字数超过该值时转为图片'),
-    background: Schema.string().role('link').description('背景图片地址，以 http(s):// 开头'),
-    blur: Schema.number().min(1).max(50).default(10).description('文本卡片模糊程度'),
-    style: Schema.string().role('textarea').default(css).description('文本卡片样式').experimental(),
-    advanced: Schema.boolean().default(false).description('是否启用高级模式（与 maxLineCount、maxLength 配置项互斥）').experimental()
+    fastify: Schema.boolean().default(false).description('快速出图模式（这会显著提高内存占用）').experimental(),
+    advanced: Schema.boolean().default(false).description('是否启用高级模式')
   }).description('基本设置'),
   Schema.union([
+    Schema.object({
+      // @ts-ignore
+      advanced: Schema.const(false),
+      maxLineCount: Schema.number().min(1).default(20).description('当文本行数超过该值时转为图片'),
+      maxLength: Schema.number().min(1).default(648).description('当返回的文本字数超过该值时转为图片'),
+    }),
     Schema.object({
       advanced: Schema.const(true).required(),
       rules: Schema.array(Schema.object({
@@ -55,28 +59,49 @@ export const Config: Schema<Config> = Schema.intersect([
         ]).description('规则计算方式'),
         righthand: Schema.string(),
         enable: Schema.boolean().default(true).description('是否启用'),
-      })).default([]).role('table').description('规则列表').experimental() as Schema<ImageRule[]>
-    }).description('高级设置') ,
-    Schema.object({})
-  ]) as Schema<Config>
+      })).default([]).role('table').description('规则列表').experimental()
+    }).description('高级设置')
+  ]) as Schema<Config>,
+  Schema.object({
+    background: Schema.string().role('link').description('背景图片地址，以 http(s):// 开头'),
+    blur: Schema.number().min(1).max(50).default(10).description('文本卡片模糊程度'),
+    style: Schema.string().role('textarea').default(css).description('文本卡片样式').experimental(),
+  }).description('卡片设置'),
 ])
 
 export const inject = ['puppeteer']
 
 export function apply(ctx: Context, config: Config) {
+  let page: Page
+  let temp: string
+
+  ctx.on('ready', async () => {
+    const temp = await readFile(require.resolve('./template.thtml'), 'utf8')
+    if (config.fastify) {
+      if (!page) page = await ctx.puppeteer.page()
+      page.setContent(template(temp, {
+        style: config.style,
+        background: config.background,
+        blur: config.blur,
+        element: '',
+        kVersion,
+        pVersion
+      }))
+    }
+  })
+
   ctx.before('send', async (session) => {
     // imagify of non platform elements
     if (session.elements.filter(e => e.type.includes(session.platform)).length === 0)
       if (h('', session.elements).toString(true).length > config.maxLength || session.elements.filter(e => ['p', 'a', 'button'].includes(e.type)).length > config.maxLineCount) {
-        const image = await ctx.puppeteer.render(template(await readFile(require.resolve('./template.thtml'), 'utf8'), {
+        const image = await ctx.puppeteer.render(await template(temp, {
           style: config.style,
           background: config.background,
           blur: config.blur,
           element: (await parser(session.elements, session)).join(''),
           kVersion,
           pVersion
-        })
-        )
+        }))
         session.elements = [...h.parse(image), ...session.elements.filter(e => appendElements.includes(e.type))]
       }
   }, true)
