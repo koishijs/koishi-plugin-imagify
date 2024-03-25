3a4e1d727b92db73f8ad9f3fc4a02b9eefd947ca
Lipraty <sis1943@outlook.com>
2024-03-25 15:31:14 +0800

    chore: add canvas rendering support

--- packages/core/src/internal/render.ts
+++ packages/core/src/internal/render.ts
@@ -1,6 +1,6 @@
-import { Context } from 'koishi'
 import { } from 'koishi-plugin-puppeteer'
 import type { Page } from 'puppeteer-core'
+import { type CanvasRenderingContext2D } from '@koishijs/canvas'
 import { PageWorker } from '../types'
 
 /// Puppeteer render
@@ -17,10 +17,9 @@ export async function pptrRender(page: Page, html: string): Promise<Page> {
 
 /**
  * Get a worker from pagepool
- * @param pagepool 
  */
-export async function pptrGetWorker(pagepool: PageWorker<Page>[]): Promise<[PageWorker<Page>[], Promise<PageWorker<Page>>]> {
-  return [pagepool, new Promise<PageWorker<Page>>((resolve) => {
+export async function pptrGetWorker(pagepool: PageWorker<Page>[]): Promise<PageWorker<Page>> {
+  return new Promise<PageWorker<Page>>((resolve) => {
     function check() {
       const available = pagepool.find(p => !p.busy)
 
@@ -32,23 +31,23 @@ export async function pptrGetWorker(pagepool: PageWorker<Page>[]): Promise<[Page
       }
     }
     check()
-  })]
+  })
 }
 
 /**
  * Screenshot page
  * @param quality image quality. maxmium hundred positive integer
  */
-export async function pptrScreenshot(page: Page, quality: number = 100): Promise<[Page, Buffer]> {
+export async function pptrScreenshot(page: Page, quality: number = 100): Promise<Buffer> {
   const { width, height } = await page.evaluate(() => {
     const element = document.querySelector('._image')
     return { width: element.clientWidth, height: element.clientHeight }
   })
-  return [page, await page.screenshot({
+  return await page.screenshot({
     clip: { x: 0, y: 0, width, height },
     quality,
     type: 'jpeg'
-  })]
+  })
 }
 
 /**
@@ -57,4 +56,5 @@ export async function pptrScreenshot(page: Page, quality: number = 100): Promise
 export const pptrClose = (page: Page) => page.close()
 
 /// Canvas render
+
 
